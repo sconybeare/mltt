e10f0fbf18b1082f30763aabe8292d841b2d3a42
Remy Goldschmidt <taktoa@gmail.com>
2016-02-20 07:26:19 -0600

    Added a Hashable instance for Variable

--- library/MLTT/Types/Variable.hs
+++ library/MLTT/Types/Variable.hs
@@ -1,6 +1,16 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric      #-}
+
 module MLTT.Types.Variable where
+
+import           Data.Data     (Data)
+import           GHC.Generics  (Generic)
+
+import           Data.Hashable
 
 data Variable = StringVar String
               | GenSym String Integer
               | Dummy
-              deriving (Show, Eq, Ord)
+              deriving (Show, Eq, Ord, Generic, Data)
+
+instance Hashable Variable
